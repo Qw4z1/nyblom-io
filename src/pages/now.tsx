21c95848db088818909efbbf9b9d4f52e36a16dd
Viktor Nyblom <viktor.nyblom@gmail.com>
2023-01-14 19:14:33 +0100

    Update Now

--- src/pages/now.tsx
+++ src/pages/now.tsx
@@ -2,14 +2,21 @@ import { readFileSync } from "fs";
 import { bundleMDX } from "mdx-bundler";
 import { getMDXComponent } from "mdx-bundler/client";
 import type { GetStaticProps, NextPage } from "next";
+import Link from "next/link";
 import { join } from "path";
-import { useMemo } from "react";
+import { DetailedHTMLProps, HTMLAttributes, ReactNode, useMemo } from "react";
 import remarkGfm from "remark-gfm";
 import { Head } from "../components/Head";
 import { Content, ContentFrontMatter } from "../types";
 interface NowPageProps {
   content: Content;
 }
+
+const UL: React.FC<
+  DetailedHTMLProps<HTMLAttributes<HTMLUListElement>, HTMLUListElement>
+> = (props) => {
+  return <ul className="space-y-2 list-disc list-inside">{props.children}</ul>;
+};
 
 const Now: NextPage<NowPageProps> = ({ content }) => {
   const lastUpdated = useMemo(
@@ -30,18 +37,16 @@ const Now: NextPage<NowPageProps> = ({ content }) => {
   return (
     <>
       <Head title={"Now"} description={"What is Viktor up to Now?"} />
-      <div className="py-4 max-w-2xl flex flex-col justify-start items-start m-auto">
+      <div className="py-4 max-w-2xl flex flex-col justify-start items-start m-auto list-disc ">
         <h1>Now</h1>
         <p className="text-base lg:text-lg">Last updated: {lastUpdated}</p>
-        <ContentItem />
+        <ContentItem components={{ Link: Link, ul: UL }} />
       </div>
     </>
   );
 };
 
-export const getStaticProps: GetStaticProps<NowPageProps> = async ({
-  params,
-}) => {
+export const getStaticProps: GetStaticProps<NowPageProps> = async () => {
   const filePath = join(process.cwd(), "content/now.mdx");
   const mdxSource = readFileSync(filePath, "utf8");
   const bundleResult = await bundleMDX({
--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -11,86 +11,74 @@ const Portfolio: NextPage = () => {
           Each of the companies below has chosen to take my money. <br /> For
           that I am forever grateful.
         </p>
-        <ul className="space-y-2">
+        <ul className="space-y-2 list-disc list-inside">
           <li>
-            &#8226;{" "}
             <a href="https://doktor.se/" className="font-bold">
               Doktor.se
             </a>{" "}
             - Talk to a doktor from anywhere.{" "}
           </li>
           <li>
-            &#8226;{" "}
             <a href="https://rgnt-motorcycles.com/" className="font-bold">
               RGNT Motorcycles
             </a>{" "}
             - Electric motorcycles.
           </li>
           <li>
-            &#8226;{" "}
             <a href="https://ljusgarda.se/" className="font-bold">
               Ljusgårda
             </a>{" "}
             - Modern farming.
           </li>
           <li>
-            &#8226;{" "}
             <a href="https://www.bzzt.se/" className="font-bold">
               Bzzt
             </a>{" "}
             - Electric pod taxi.
           </li>
           <li>
-            &#8226;{" "}
             <a href="https://www.miltrain.se/" className="font-bold">
               Miltrain
             </a>{" "}
             - Get better results on your training journey.
           </li>
           <li>
-            &#8226;{" "}
             <a href="http://molistreaming.com/" className="font-bold">
               Moli Streaming
             </a>{" "}
             - Next generation social network.
           </li>
           <li>
-            &#8226;{" "}
             <a href="https://detecht.se/" className="font-bold">
               Detecht
             </a>{" "}
             - The lifesaving motorcycle app.
           </li>
           <li>
-            &#8226;{" "}
             <a href="https://babyjourney.se/" className="font-bold">
               Baby Journey
             </a>{" "}
             - The modern app for pregnant and parents of young children.
           </li>
           <li>
-            &#8226;{" "}
             <a href="https://www.doneservices.se/" className="font-bold">
               Done
             </a>{" "}
             - The easy way to find a handyman.
           </li>
           <li>
-            &#8226;{" "}
             <a href="https://shinner.app/" className="font-bold">
               Shinner
             </a>{" "}
             - Explore thousands of skateboard spots worldwide.
           </li>
           <li>
-            &#8226;{" "}
             <a href="https://microshifts.app/" className="font-bold">
               Microshifts
             </a>{" "}
             - Helps you do more of what matters most.
           </li>
           <li>
-            &#8226;{" "}
             <a href="https://www.lectrium.io/" className="font-bold">
               Lechtrium
             </a>{" "}
