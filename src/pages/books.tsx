b8e3e2b6a6a269b2f9a7add57a9a6a3c3245bb51
Viktor Nyblom <viktor.nyblom@gmail.com>
2023-02-19 09:47:59 +0100

    Fix table for Books page

--- src/pages/books.tsx
+++ src/pages/books.tsx
@@ -4,72 +4,58 @@ import getBooks from "../helpers/getBooks";
 import { Book } from "../types/books";
 
 interface BooksPageProps {
-  books: {
-    [key: string]: Book[];
-  };
+  books: Book[];
 }
+export const getStaticProps: GetStaticProps<BooksPageProps> = async () => {
+  const books = await getBooks();
+  const sorted = books.sort((a, b) => a.name.localeCompare(b.name));
+
+  return {
+    props: {
+      books: sorted,
+    },
+  };
+};
 
 const Now: NextPage<BooksPageProps> = ({ books }) => {
   return (
     <>
-      <Head
-        title={"Books"}
-        description={
-          "Viktor's top picks"
-        }
-      />
+      <Head title={"Books"} description={"Viktor's top picks"} />
       <div className="py-4 w-full flex flex-col justify-start items-start m-auto">
         <h1>Books</h1>
         <p className="text-base lg:text-lg">
           These are the books I most commonly recommend, neatly organized by
-          category. Each on of them is rated 4 (out of 5) or higher in my spreadsheet of
-          books I&apos;ve read, so you don&apos;t have to wonder if it&apos;s
-          worth reading.
+          category. Each on of them is rated 4 (out of 5) or higher in my
+          spreadsheet of books I&apos;ve read, so you don&apos;t have to wonder
+          if it&apos;s worth reading.
         </p>
-        <div>
-          {Object.keys(books)
-            .sort()
-            .map((key: string) => {
-              const category = books[key];
+        <table className="table-fixed border-separate border-spacing-y-4 border-spacing-x-2">
+          <thead className="text-left">
+            <tr>
+              <th>Name</th>
+              <th>Author</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          <tbody>
+            {books.map((book: Book) => {
               return (
-                <div key={key}>
-                  <h3>{key}</h3>
-                  <ul className="list-disc list-inside">
-                    {category.map((it) => {
-                      return (
-                        <li className="pt-2" key={it.link}>
-                          <a href={it.link}>
-                            <span className="capitalize">{it.name}</span>
-                          </a>{" "}
-                          by {it.author}
-                        </li>
-                      );
-                    })}
-                  </ul>
-                </div>
+                <tr key={book.id} className="border">
+                  <td>
+                    <a href={book.link} className="capitalize">
+                      {book.name}
+                    </a>
+                  </td>
+                  <td>{book.author}</td>
+                  <td>{book.category}</td>
+                </tr>
               );
             })}
-        </div>
+          </tbody>
+        </table>
       </div>
     </>
   );
-};
-
-export const getStaticProps: GetStaticProps<BooksPageProps> = async () => {
-  const books = await getBooks();
-
-  const categorizedBooks = books.reduce((result: any, current) => {
-    return {
-      ...result,
-      [current.category]: [...(result[current.category] ?? []), current],
-    };
-  }, []);
-
-  return {
-    props: {
-      books: categorizedBooks,
-    },
-  };
 };
 
 export default Now;
