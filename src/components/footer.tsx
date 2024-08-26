9e8c43cfc27b0c8268821c36abbb323b8dadbf7c
Viktor Nyblom <viktor.nyblom@gmail.com>
2024-08-26 08:46:59 +0200

    Fix footer padding

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -7,7 +7,7 @@ export interface FooterProps {}
 const Footer: FC<FooterProps> = () => {
   return (
     <div className="w-full">
-      <div className="flex flex-row w-full flex-auto justify-between container border-t p-4">
+      <div className="flex flex-row w-full flex-auto justify-between container border-t p-12">
         <div className="flex flex-col">
           <p className="italic text-md">© 2023 Viktor Nyblom</p>
         </div>
