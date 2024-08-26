0491eecac40a46d2d7598f7bc29ea1effe44d8d8
Viktor Nyblom <viktor.nyblom@gmail.com>
2024-08-26 08:47:24 +0200

    Update copyright year

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -9,7 +9,7 @@ const Footer: FC<FooterProps> = () => {
     <div className="w-full">
       <div className="flex flex-row w-full flex-auto justify-between container border-t p-12">
         <div className="flex flex-col">
-          <p className="italic text-md">© 2023 Viktor Nyblom</p>
+          <p className="italic text-md">© 2024 Viktor Nyblom</p>
         </div>
         <div className="flex flex-row justify-center align-middle">
           <SocialRow />
