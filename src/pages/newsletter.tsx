2d6aa3e7cbb9d18b6434d58e1e288a14970a4515
Viktor Nyblom <viktor.nyblom@gmail.com>
2024-09-12 08:00:20 +0200

    Update copy

--- src/pages/newsletter.tsx
+++ src/pages/newsletter.tsx
@@ -7,7 +7,7 @@ const Newsletter: NextPage = () => {
     <>
       <Head
         title="Viktor's Weekly"
-        description="Writing about the intersection of strategy, product and tech"
+        description="Lessons learned from a decade of building apps, teams and companies"
       />
       <div className="max-w-2xl flex flex-col justify-start items-start m-auto pb-10">
         <h1 className="mb-12">
@@ -15,28 +15,26 @@ const Newsletter: NextPage = () => {
         </h1>
 
         <h2 className="mb-8">
-          Every Friday, I deliver straight to your inbox:
+          Every Friday, I deliver straight to your inbox lessons learned from
+          over a decade of building apps, teams and companies.
         </h2>
 
+        <p>This includes: </p>
         <ul className="list-disc pl-6 mb-12 space-y-4">
-          <li>
-            My lastest thoughts in the intersection of strategy, product, and
-            tech
-          </li>
-          <li>
-            Actionable advice from a decade of building apps, teams, and
-            companies
-          </li>
-          <li>Thought-provoking ideas to elevate your professional game</li>
+          <li>Actionable advice about how to build a company</li>
+          <li>My fravorite frameworks for building product</li>
           <li>
             Links to the most interesting things I&apos;ve found this week
           </li>
+          <li>Stories from companies I&apos;ve worked on, in and with</li>
+          <li>The odd philosophical take</li>
         </ul>
 
         <InlineNewsletterForm />
 
         <p className="italic mt-12">
-          &quot;I have subscribed to 1000s of newsletters and this is by far my favorite!&quot; - Viktor Nyblom
+          &quot;I have subscribed to 1000s of newsletters and this is by far my
+          favorite!&quot; - Viktor Nyblom
         </p>
       </div>
     </>
