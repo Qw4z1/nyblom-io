d53e43c850308a904287f560200709d1694ed1c0
Viktor Nyblom <viktor.nyblom@gmail.com>
2025-03-13 16:04:31 +0100

    Updated testimonials

--- src/components/testimonials/util.ts
+++ src/components/testimonials/util.ts
@@ -38,13 +38,31 @@ export const clarityCallTestimonials: iTestimonial[] = [
 
 export const naasTestimonials: iTestimonial[] = [
   {
+    name: "Simon Chobot",
+    link: "https://www.linkedin.com/in/simon-chobot",
+    title: "CTO @ SUNT",
+    short:
+      "Working with Viktor has made it so that I no longer have to constantly think about what to do next—my actions are automatically guided by the bigger goal. Working with Viktor has opened up new perspectives and given me a deeper understanding of my opportunities. He’s a great person to bounce ideas off of or use as a sounding board. I would absolutely recommend working with Viktor to anyone looking for practical, results-driven guidance.",
+    quote:
+      "Working with Viktor has made it so that I no longer have to constantly think about what to do next—my actions are automatically guided by the bigger goal. When I start something, it just clicks, and I work more effectively with the end result in mind. At the same time, collaborating with Viktor has opened up new perspectives and given me a deeper understanding of my opportunities. He’s a great person to bounce ideas off of or use as a sounding board. I would absolutely recommend working with Viktor to anyone looking for practical, results-driven guidance.",
+  },
+  {
+    name: "Hannes Widetag",
+    link: "https://x.com/_carlhannes/status/1830957145409442110",
+    title: "Founder @ AsekioAI",
+    short:
+      "Just had an amazing Clarity call with Viktor. In the beginning I wasn't sure what I was asking about, but the output was a clear path forward 🚀.Sometimes you don't actually need a fully formed question, just the idea that you need help, I'm glad I made something of it",
+    quote:
+      "Just had an amazing Clarity call with Viktor. In the beginning I wasn't sure what I was asking about, but the output was a clear path forward 🚀.Sometimes you don't actually need a fully formed question, just the idea that you need help, I'm glad I made something of it",
+  },
+  {
     name: "Neo Moreton",
     link: "https://www.linkedin.com/in/neomoreton/",
     title: "(AI) Founder | Executive Coach | Author",
     short:
       "Viktor's approach expands thinking and encourages exploring different avenues, leading to greater opportunities and risk mitigation. As a team player, he effectively engages stakeholders to propel ideas forward.",
     quote:
-      "I have had the pleasure of knowing Viktor Nyblom for four years, and we have collaborated in various capacities. Personally, I find Viktor to be incredibly likable and easy-going. Professionally, his experience, strategic mindset, and willingness to challenge the status quo have been invaluable. Viktor's approach expands thinking and encourages exploring different avenues, leading to greater opportunities and risk mitigation. As a team player, he effectively engages stakeholders to propel ideas forward. With his action-oriented nature, he steadily  sets directions, commits to plans, and executes with solid results. I highly recommend Viktor as a fractional CTO and strategic advisor for anyone seeking innovative solutions and exceptional guidance.",
+      "I have had the pleasure of knowing Viktor Nyblom for four years, and we have collaborated in various capacities. Personally, I find Viktor to be incredibly likable and easy-going. Professionally, his experience, strategic mindset, and willingness to challenge the status quo have been invaluable. Viktor's approach expands thinking and encourages exploring different avenues, leading to greater opportunities and risk mitigation. With his action-oriented nature, he steadily helps to set directions, commits to plans, and execute with solid results. I highly recommend Viktor as a fractional CTO and strategic advisor for anyone seeking innovative solutions and exceptional guidance.",
   },
   {
     name: "Charana Amarasekara",
@@ -53,15 +71,6 @@ export const naasTestimonials: iTestimonial[] = [
     short:
       "Viktor has a gift for bringing out the best in his team. Despite working remotely with developers overseas, he quickly grasped everyone's strengths and assigned tasks accordingly. Under his direction, we delivered features to market at record speed.",
     quote:
-      "Viktor has a gift for bringing out the best in his team. Despite working remotely with developers overseas, he quickly grasped everyone's strengths and assigned tasks accordingly. Under his direction, we delivered features to market at record speed.\nBeyond his technical and management expertise, Viktor genuinely cares about the wellbeing of each team member. He has coached and motivated me through challenges, while celebrating accomplishments.\nI'm thankful for the chance to learn from this talented, compassionate leader. Viktor has made an immense impact on my career. It's leaders like him that make a company great.",
-  },
-  {
-    name: "Timothy Forni",
-    link: "https://www.linkedin.com/in/timothy-forni-809a2293/",
-    title: "COO & Co-founder Baby Journey",
-    short:
-      "Viktor was brought in to lead the transition from agency to inhouse team and worked in the management team for 2.5 years. His ability to explain technical challenges in simple terms was a huge asset to us founders.",
-    quote:
-      "Viktor was brought in to lead the transition from agency to inhouse team and worked in the management team for 2.5 years. His ability to explain technical challenges in simple terms was a huge asset to us founders. Viktor left a legacy in the form of a solid foundation in terms of running the techorganisation that now enables us to scale rapidly. I've lost count of the number of times I've said to other parts of the company to look to the tech org for inspiration on processes.",
+      "Viktor has a gift for bringing out the best in teams. Despite working remotely with developers overseas, he quickly grasped everyone's strengths and assigned tasks accordingly. Under his direction, we delivered features to market at record speed.\nBeyond his technical and management expertise, Viktor genuinely cares about the wellbeing of each team member. He has coached and motivated me through challenges, while celebrating accomplishments.\nI'm thankful for the chance to learn from this talented, compassionate leader. Viktor has made an immense impact on my career. It's leaders like him that make a company great.",
   },
 ];
